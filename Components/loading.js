935abb7beaf017fb104be561f001839dcf41adb5
mahoneythomas75@gmail.com <mahoneythomas75@gmail.com>
2024-05-20 12:29:36 +0100

    New

--- Components/loading.js
+++ Components/loading.js
@@ -1,4 +1,4 @@
-const loadingHTML = `<div class="loadingChild w-full h-full fixed bg-dark z-[60] flex justify-center items-center"> <img class="w-[10vw]" src="../assets/logo/logo.svg" alt=""> </div>`;
+const loadingHTML = `<div class="loadingChild w-full h-full fixed bg-dark z-[60] flex justify-center items-center"> <img class="w-[30vw] sm:w-[12vw]" src="../assets/logo/logo.svg" alt=""> </div>`;
 
 let loadingQuery = document.querySelector(".loading");
 loadingQuery.innerHTML = loadingHTML;
