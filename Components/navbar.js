582a8915f008086875bd42ab9a078f460fea6a16
mahoneythomas75@gmail.com <mahoneythomas75@gmail.com>
2024-05-21 23:07:00 +0100

    new

--- Components/navbar.js
+++ Components/navbar.js
@@ -18,9 +18,7 @@ const navbarHTML = `<nav class="navbarchild border-gray-200d h-auto bg-transpare
       <li>
         <a href="../../index.html" class="block py-2 px-3 md:p-0 text-white  rounded md:bg-transparent  aria-current="page">Home</a>
       </li>
-      <li>
-        <a href="../../index.html#Projects" class="block py-2 px-3 md:p-0 rounded md:text-white   md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Projects</a>
-      </li>
+    
       <li>
         <a href="../../index.html#Services" class="block py-2 px-3 md:p-0 rounded md:text-white   hover:text-blue-500 text-white hover:bg-gray-700 md:hover:bg-transparent border-gray-700">Services</a>
       </li>
