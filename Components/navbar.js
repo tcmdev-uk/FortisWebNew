184a038e7210ee6b6a56f70d856a6c0484ee0a7d
mahoneythomas75@gmail.com <mahoneythomas75@gmail.com>
2024-05-21 07:07:15 +0100

    new

--- Components/navbar.js
+++ Components/navbar.js
@@ -5,7 +5,7 @@ const navbarHTML = `<nav class="navbarchild border-gray-200d h-auto bg-transpare
      
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button onclick="window.location.href = '../../Pages/contactus/contactUs.html'"type="button" class="text-white bg-mainOrange hover:bg-darkOrange hidden md:block focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center  focus:ring-blue-800">Get started</button>
+      <button onclick="window.location.href = '../../Pages/contactus/contactUs.html'"type="button" class="text-white bg-mainOrange hover:bg-darkOrange  md:block focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center  focus:ring-blue-800">Get started</button>
       <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2" aria-controls="navbar-cta" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
